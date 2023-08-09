39253288fbddc7ab0a0420bd68910bdd59c8c906
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-08-09 19:46:56 -0400

    create database "online_store"

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'catalog.apps.CatalogConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_store',
+        'USER': 'postgres',
+        'PASSWORD': '20010906',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
