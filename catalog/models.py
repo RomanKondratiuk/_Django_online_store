9cfac691496b5e6cbdc2d664c196575cf22a8ec4
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-08-09 20:56:47 -0400

    created descriptions for models

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='name ')
+    description = models.TextField(verbose_name='description')
+    image = models.ImageField(upload_to='image/', verbose_name='image', **NULLABLE)
+    category = models.TextField(verbose_name='category')
+    purchase_price = models.IntegerField()
+    date_of_creation = models.DateField()
+    last_modified_date = models.DateField()
+
+    def __str__(self):
+        print(f'{self.name} {self.description}')
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+        ordering = ('name',)
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='name ')
+    description = models.TextField(verbose_name='description')
+
+    def __str__(self):
+        print(f'{self.name}')
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ('name',)
