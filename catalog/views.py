c927735c0c03e36a97b5c939420981bb2856353e
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-08-17 07:46:14 -0400

    вроде работает

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def contacts(request):
     if request.method == 'POST':
@@ -15,9 +17,8 @@ def home(request):
 
 
 def products(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f"Имя пользователя: {name} \nEmail пользователя:{email}\nСообщение пользователя: {message}")
-    return render(request, 'catalog/products.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list
+    }
+    return render(request, 'catalog/products.html', context)
