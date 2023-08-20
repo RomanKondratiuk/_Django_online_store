938b57445a0e7547a4375f0295b5cb7d5ff975e8
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-08-19 21:44:41 -0400

    add photo

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from catalog.apps import CatalogConfig
 from catalog.views import contacts, products, base
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', base, name='base'),
