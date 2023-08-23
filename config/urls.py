aa4f0fa4cfff056b1c2cdaab947be53e2b9d25ad
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-08-23 19:12:49 -0400

    add CREATE

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('blogpost/', include('blogpost.urls', namespace='blogpost')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
